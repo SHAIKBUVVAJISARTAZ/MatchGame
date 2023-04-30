ab3834990605bebce2d9f262a5ff78c4ef38ee82
Sartaz Shaik Buvvaji <buvvajisartaz373@gmail.com>
2023-04-30 15:42:59 +0530

    completed coding for complete game

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -15,16 +15,34 @@ using System.Windows.Shapes;
 
 namespace MatchGame
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+            
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if(matchesFound==8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " -Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -44,16 +62,52 @@ namespace MatchGame
 
             foreach(TextBlock textblock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textblock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                if (textblock.Name != "timeTextBlock")
+                {
+                    textblock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textblock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
             }
+            timer.Start();
+            tenthsOfSecondsElapsed= 0;
+            matchesFound= 0;
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
 
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if(findingMatch==false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if(textBlock.Text==lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch= false;
+                matchesFound++;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility= Visibility.Visible;
+                findingMatch = false;
+            }
+
+        }
+
+        private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(matchesFound==8)
+            {
+                SetUpGame();
+            }
         }
     }
 }
